edd8845000ebe539a28b660323697594337b1445
Frank <frank.next@gmx.de>
2017-08-03 22:14:42 +0200

    added websocket stubs for later use

---
+++ js/data/Datasource.js
@@ -0,0 +1,25 @@
+/**
+ * what we want is a simple baisc abstraction layer to retrieve data .. nothing fancy just some structure for different services and probably topics
+ *
+ *
+ *
+ */
+
+import io from 'socket.io-client'
+
+export default
+class Datasource
+{
+
+    constructor(serviceURL){
+
+        //TODO add some listeners to retrieve data about current stock prices and news
+
+        // Connect to our node/websockets server
+        var socket =this.mSocket=io.connect(serviceURL);
+
+
+    }
+
+
+}
