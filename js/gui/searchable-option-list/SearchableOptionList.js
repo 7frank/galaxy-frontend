9bde10b36da838e6cb3e8b14e9d92489ead97014
Frank <frank.next@gmx.de>
2017-08-11 00:56:14 +0200

    webcomponent: searchable-option-list

---
+++ js/gui/searchable-option-list/SearchableOptionList.js
@@ -0,0 +1,36 @@
+
+import "./sol"
+import "./sol.css"
+import searchListOoptions from "./searchOptions.json"
+
+
+import template from "./searchable-option-list.html"
+
+class SearchableOptionList extends HTMLElement {
+
+
+    connectedCallback() {
+
+        $(this).append(template)
+            .searchableOptionList({
+                maxHeight: '250px', showSelectAll: false,
+                data: searchListOoptions,
+                converter: function (sol, rawDataFromUrl) {
+                    var solData = rawDataFromUrl;
+
+                    // do whatever you have to do
+                    // to convert rawDataFromUrl to
+                    // valid SOL data format
+
+                    return solData;
+                }
+            });
+    }
+
+
+
+
+}
+
+
+customElements.define("searchable-option-list", SearchableOptionList,{extends:"select"});
