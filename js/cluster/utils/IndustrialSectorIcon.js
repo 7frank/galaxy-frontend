63ab5c7357ad1854fa234a897515093dcdaf8676
Frank <frank.next@gmx.de>
2017-08-15 13:06:55 +0200

    two small helpers for rendering of the industrial sector names

---
+++ js/cluster/utils/IndustrialSectorIcon.js
@@ -0,0 +1,21 @@
+//import ""
+
+
+export function IndustrialSectorIcon(sectorName) {
+    return `<img src="./img/industryIcons/${sectorName}.png" title="${sectorName}" />`
+
+
+}
+
+export
+function IndustrialSectorAbbreviation(sectorName) {
+
+    sectorName= _.startCase(sectorName)
+
+    if (sectorName.indexOf(" ")==-1) return sectorName
+
+    return sectorName.replace(/[a-z\s]/g, '');
+
+}
+
+
