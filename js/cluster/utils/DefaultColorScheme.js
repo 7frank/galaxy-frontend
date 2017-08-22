7611736f3f5158c3bae030fe9fc3a742d8ee9255
Frank <frank.next@gmx.de>
2017-08-22 14:17:59 +0200

    minor changes to gui components

---
+++ js/cluster/utils/DefaultColorScheme.js
@@ -0,0 +1,62 @@
+/**
+ *
+ * manage color for cluster
+ * have listeners so that listeners can handle changes
+ *
+ *
+ */
+import Color from 'easy-color';
+//OptionsManager
+// colors
+export default
+class DefaultColorScheme extends HTMLElement {
+
+
+    static get observedAttributes() {return ['edge-color',"background-color"]; }
+
+    // Respond to attribute changes.
+    attributeChangedCallback(attr, oldValue, newValue) {
+
+
+        var parser = new Color(newValue); // You can also add: # 0af, rgb (0, 170, 255), hsl (..., etc ...
+
+        newValue=  parser.toRGBA();
+        if (oldValue!=newValue)
+        this.trigger(attr+"-changed",[newValue,oldValue ])
+    }
+
+
+    on(evntName,handler){
+        $(this).on(evntName,handler.bind(this))
+        return this
+    }
+
+    trigger(evntName,args){
+        $(this).trigger(evntName,args)
+        return this
+    }
+
+
+    //have some utils to convert colors
+    //maybe generate materials also
+
+   constructor(){
+        super();
+
+
+       $(this).on("edge-color-changed",function(){
+           console.warn(arguments)
+
+
+       })
+
+
+       this.setAttribute("edge-color","#00AAFFFF")
+       this.setAttribute("background-color","black")
+
+   }
+
+}
+
+customElements.define("default-color-scheme",DefaultColorScheme)
+
---
+++ js/gui/company-details/CompanyDetails.js
@@ -0,0 +1,102 @@
+
+import bodyHTML from "./company-details-body.html"
+
+class CompanyDetails extends HTMLElement {
+
+    constructor(...args) {
+        super(...args);
+
+
+
+
+    }
+
+    connectedCallback(){
+
+
+        $(this).append(bodyHTML)
+
+
+
+    }
+
+
+
+
+    //placeholder use polymer highstock component instead if possible
+    initBody()
+    {
+        dj_recent();
+        var seriesOptions = [],seriesCounter = 0,names = ['stockprice', 'sentiment'];
+        function createChart() {
+            Highcharts.stockChart(
+                'max-chart',
+                {
+                    chart: {
+                        style: {
+                            fontFamily: 'robotoCondensed-light'
+                        }
+                        ,
+                        backgroundColor:'rgba(0, 0, 0, 0.31)'
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    rangeSelector: {
+                        selected: 4,
+                        enabled:true,
+                        inputEnabled: $('#max-chart').width() > 300
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        labels: {
+                            format: '{value:%m/%e}',
+                            align: 'center'
+                        }
+                    },
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                            }
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 2,
+                            color: 'silver'
+                        }]
+                    },
+                    navigator: {
+                        enabled: true,
+                        height: 20
+                    },
+                    plotOptions: {
+                        series: {
+                            compare: 'percent',
+                            showInNavigator: true
+                        }
+                    },
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2,
+                        split: true
+                    },
+                    scrollbar: {
+                        enabled: false
+                    },
+                    series: seriesOptions,
+                    exporting: {
+                        enabled: false
+                    }
+                });
+        }
+
+
+    }
+
+
+
+}
+
+
+customElements.define("company-details", CompanyDetails);
