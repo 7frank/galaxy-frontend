40ab7a4e4a0e053e3d21996ef16f2c7f3633824b
Frank <frank.next@gmx.de>
2017-08-25 21:34:24 +0200

    added template strings

---
+++ js/utils/TemplateString.js
@@ -0,0 +1,22 @@
+
+
+import _ from "lodash"
+
+
+export default
+class TemplateString
+{
+    constructor(str)
+    {
+
+        this.mStr=str;
+
+    }
+
+
+    format(obj) {
+            return _.template(_.isString(this.mStr) ? this.mStr : '', { interpolate: /\$\{([^\}]+)\}/gm })(obj);
+    }
+
+
+}
