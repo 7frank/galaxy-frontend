934cd8475178eb801ec4be2ca75c36f2033bf43e
Frank <frank.next@gmx.de>
2017-09-10 21:44:25 +0200

    refactored mesh edges into ClusterBaseEdges and added ClusterMeshEdges which is an implementation of THREE.Meshline for our edges to support line-width

---
+++ js/cluster/edges/ClusterBaseEdges.js
@@ -0,0 +1,287 @@
+import EdgeUtil from "../EdgeUtil";
+import MaterialFadeMixin from "../../utils/MaterialFadeMixin";
+import BaseCluster3D from "../BaseCluster3D";
+
+/**
+ * the default implementation for the cluster-to-neighboring-clusters edges
+ *
+ *
+ * simple/fast/no width support (line width = 1)
+ *
+ * usage:
+ * initialize using: setClusters()
+ * run the drawing process: update()
+ * TODO add support to only recreate cluster edges if clusters have changed this way  supporting streaming clusters in the future
+ */
+
+
+
+
+export default class ClusterBaseEdges extends THREE.Line {
+
+
+    constructor(siblingClustersObj, materialOptions) {
+        super();
+
+        if (siblingClustersObj)
+            this.setClusters(siblingClustersObj);
+
+        this.name = "EdgesElement";
+
+        this.initEdgeMesh(materialOptions)
+
+
+    }
+
+    getDefaultMaterial(options) {
+
+
+        let defaults = {
+            opacity: 1.0,
+            transparent: true,
+            //lineIsVisible:true, // if disabled the line won't be shown on the scene
+            color: 0x999999
+        };
+
+        options = _.extend(defaults, options);
+
+
+        var lineMaterial = new THREE.LineBasicMaterial({
+            color: options.color,
+            transparent: options.transparent,
+            opacity: options.opacity,
+            depthTest: true,
+            depthWrite: false//,
+            //depthFunc:THREE.NeverDepth
+        });
+
+
+        //   FIXME lines should not interfere with it's cluster (currently are overdrawing)
+        /*
+          lineMaterial = this.getShaderLineMaterial();
+          lineMaterial.color = new THREE.Color(options.color);
+          lineMaterial.opacity = options.opacity;
+
+          if (!window["lineMaterial"]) window["lineMaterial"] = []
+          window["lineMaterial"].push(lineMaterial)
+
+          */
+
+
+        MaterialFadeMixin(lineMaterial);
+
+
+        return lineMaterial;
+
+    }
+
+
+    getShaderLineMaterial() {
+
+
+        let fragmentShader = `
+        
+        	uniform vec3 color;
+			uniform float opacity;
+
+			//varying vec3 vColor;
+
+			void main() {
+
+            //if (gl_FragCoord.z > 0.001) discard;
+            if (gl_FragColor.w > 0.5) discard;
+
+
+				gl_FragColor = vec4(// vColor *
+                 color,opacity*gl_FragCoord.z );
+
+			}
+        
+        `;
+
+
+        let attributes = {
+
+            displacement: {type: 'v3', value: []},
+            customColor: {type: 'c', value: []}
+
+        };
+
+        let uniforms = {
+
+            amplitude: {type: "f", value: 5.0},
+            opacity: {type: "f", value: 0.3},
+            color: {type: "c", value: new THREE.Color(0xff0000)}
+
+        };
+
+        var lineMaterial = new THREE.ShaderMaterial({
+
+            uniforms: uniforms,
+            // attributes:     attributes,
+            //  vertexShader:   vertexShader,
+            fragmentShader: fragmentShader,
+            blending: THREE.AdditiveBlending,
+            depthTest: false,
+            transparent: true
+
+        });
+
+        lineMaterial.linewidth = 1;
+
+
+        lineMaterial._color = lineMaterial.color;
+        Reflect.defineProperty(lineMaterial, "color", {
+            enumerable: false,
+            configurable: false,
+            get: function () {
+                return this._color
+            },
+            set: function (c) {
+
+                this._color = c;
+                this.uniforms.color.value = c
+
+            }
+        });
+
+
+        return lineMaterial
+    }
+
+    /**
+     * TODO have a mechaminsm that resets this stae in case other clsuters get streamed afterwards
+     *
+     *
+     * @param clusters
+     */
+    createEdgesForClusters(clusters) {
+
+        if (this.mChildClustersEdges) return this.mChildClustersEdges;
+
+        return this.mChildClustersEdges = EdgeUtil.createEdgesBetweenClustersFromMap(clusters);
+
+    }
+
+    /**
+     * generated and updates edges between clusters
+     *
+     */
+    initEdgeMesh(options) {
+
+
+        var line_geom = new THREE.Geometry();
+
+        var lineMaterial = this.getDefaultMaterial(options);
+
+
+        // this.mChildClustersEdgesMesh = new THREE.Line(line_geom, lineMaterial, THREE.LineSegments);
+        this.geometry = line_geom;
+        this.material = lineMaterial;
+
+        //TODO check if this might be helpful to put edges behind nodes
+
+        this.renderOrder = -2;
+
+
+        this.geometry.boundingBox = new THREE.Box3;
+        this.geometry.boundingSphere = new THREE.Sphere(new THREE.Vector3, 1);
+
+
+        lineMaterial.fade = 0;
+        lineMaterial.fadeTo(1, 2000)
+
+
+    }
+
+    setClusters(clusters) {
+        this.mClusters = clusters
+
+    }
+
+
+    getPositionForElement(el) {
+        let pos;
+        if (el.mExpanded == true) {
+            //expanded: we use the bounding box of the hull if it exists
+            if (el.mHull)
+                pos = el.mHull.mBoundingBox.getCenter();
+
+
+        }
+        else {
+            //collapsed: we use the center of the bBox that should have been set when creating the mCollapsedClusterHull
+            if (el.mCollapsedClusterHull)
+            //pos = el.geometry.boundingBox.getCenter();
+                pos = el.mCollapsedClusterHull.position.clone();
+
+
+        }
+
+        if (!pos) pos = new THREE.Vector3;
+
+        //above elements do only contain the relative position to its container, so we'll add the position of the cluster
+        pos.add(el.position);
+
+        return pos
+
+    }
+
+
+    /**
+     * updates the edges of the clusters as soon as the hull feature is rendered
+     *
+     *
+     */
+
+
+    update() {
+
+
+        let edges = this.createEdgesForClusters(this.mClusters);
+
+
+        var line_geom = new THREE.Geometry();
+
+
+        this.geometry.dispose();
+        this.geometry = line_geom;
+
+
+        var invalidEdges = [];
+
+        for (let edge of edges) {
+
+            let src, dst;
+
+            let s, d;
+            s = edge.source instanceof BaseCluster3D ? edge.source : edge.source._el;
+            d = edge.target instanceof BaseCluster3D ? edge.target : edge.target._el;
+
+            if (!s || !d) {
+                invalidEdges.push(edge);
+                continue
+            }
+
+            src = this.getPositionForElement(s);
+            dst = this.getPositionForElement(d);
+
+            //Note: currently there is no need to cut off edges because they are only drawn from src to dest
+            //cut off dst at 50% because the element should occure twice
+            // let l_50=dst.clone().sub(src).multiplyScalar(0.5)
+            // dst.sub(l_50)
+
+            line_geom.vertices.push(src);
+            line_geom.vertices.push(dst);
+
+        }
+
+
+        if (invalidEdges.length > 0)
+            console.error("invalid edges", invalidEdges)
+
+
+    }
+
+
+}
---
+++ js/cluster/edges/ClusterMeshEdges.js
@@ -0,0 +1,169 @@
+import MaterialFadeMixin from "../../utils/MaterialFadeMixin";
+import ClusterBaseEdges from "./ClusterBaseEdges";
+import {MeshLine, MeshLineMaterial} from "three.meshline"
+import BaseCluster3D from "../BaseCluster3D";
+
+/**
+ * the default implementation for the cluster-to-neighboring-clusters edges
+ *
+ *
+ * simple/fast/no width support (line width = 1)
+ *
+ */
+
+
+
+
+export default class ClusterMeshEdges extends ClusterBaseEdges {
+
+    constructor(...args) {
+        super(...args)
+
+        this.smoothenWidth = true;
+
+    }
+
+
+    getDefaultMaterial(options) {
+
+
+        let defaults = {
+            opacity: 1.0,
+            transparent: true,
+            //lineIsVisible:true, // if disabled the line won't be shown on the scene
+            color: 0x999999
+        };
+
+        options = _.extend(defaults, options);
+
+
+        //TODO wireframe meshlines to check if minimal triangle count
+        var material = new MeshLineMaterial({
+            lineWidth: 1,
+            color: new THREE.Color(options.color),
+            transparent: options.transparent,
+            opacity: options.opacity,
+        });
+
+        MaterialFadeMixin(material);
+
+
+        return material;
+
+    }
+
+
+    update() {
+
+
+        //remove previously generated MeshLines
+
+        for (var i = this.children.length - 1; i >= 0; i--) {
+            this.remove(this.children[i]);
+        }
+
+
+        //------------------------------------
+
+        let edges = this.createEdgesForClusters(this.mClusters);
+
+
+        var line_geom = new THREE.Geometry();
+
+
+        this.geometry.dispose();
+        this.geometry = line_geom;
+
+
+        var invalidEdges = [];
+
+        for (let edge of edges) {
+            //TODO we should unify the edges to not always have 2 separate ways to access certain elements
+            //TODO also we should use the center of the hull feature instead
+            //FIXME for cluster: add edges only if mHull exists
+
+            let src, dst;
+
+            let s, d;
+            s = edge.source instanceof BaseCluster3D ? edge.source : edge.source._el;
+            d = edge.target instanceof BaseCluster3D ? edge.target : edge.target._el;
+
+            if (!s || !d) {
+                invalidEdges.push(edge);
+                continue
+            }
+
+            src = this.getPositionForElement(s);
+            dst = this.getPositionForElement(d);
+
+            //Note: currently there is no need to cut off edges because they are only drawn from src to dest
+            //cut off dst at 50% because the element should occure twice
+            // let l_50=dst.clone().sub(src).multiplyScalar(0.5)
+            // dst.sub(l_50)
+
+            // line_geom.vertices.push(src);
+            //  line_geom.vertices.push(dst);
+
+            //----------------------------------
+            //TODO have an option to change the line implementation
+            var geometry = new THREE.Geometry();
+            geometry.vertices.push(src);
+
+
+            let widthFN;
+            if (this.smoothenWidth == false)
+                geometry.vertices.push(dst);
+            else {
+
+                // smoothing the line with like in the demo
+
+                for (let i = 0; i <= 1; i += 0.1)
+                    geometry.vertices.push(src.clone().lerp(dst, i));
+
+                function parabola(x, k) {
+                    return Math.pow(4 * x * ( 1 - x ), k);
+                }
+
+                widthFN=function widthFunction(p) {
+                    return 1 * parabola(p, 1)
+                }
+            }
+
+            var meshLine = new MeshLine();
+            meshLine.setGeometry(geometry, widthFN);
+
+
+            //TODO
+            var material = new MeshLineMaterial({
+                lineWidth: edge.link_strength || 1,
+                color: new THREE.Color(0x333333),
+                transparent: true,
+                opacity: 0.5
+            });
+            // var material = new MeshLineMaterial();
+
+
+            //TODO material group for lineWidth feature + only one material
+            //  material.copy(this.material);
+            // material.lineWidth = edge.link_strength;
+
+
+            if (edge.link_strength == undefined)
+                console.error("edge does not have a link_strength''")
+
+
+            var mesh = new THREE.Mesh(meshLine.geometry, material); // this syntax could definitely be improved!
+            this.add(mesh);
+
+
+        }
+
+
+        if (invalidEdges.length > 0)
+            console.error("invalid edges", invalidEdges)
+
+
+    }
+
+
+}
