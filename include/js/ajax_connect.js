79bf31659412e614f82ed929781c536cb0f64a60
Frank <frank.reimann@jambit.de>
2026-07-22 22:32:16 +0200

    remove stuff

--- include/js/ajax_connect.js
+++
@@ -1,8 +0,0 @@
-function Inint_AJAX() 
-{
-	try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch(e) {}
-	try { return new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
-	try { return new XMLHttpRequest(); } catch(e) {}
-	alert("XMLHttpRequest not supported");
-	return null;
-}
