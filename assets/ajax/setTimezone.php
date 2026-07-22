56c2f4edc909cefcc0836bdb015b2cc933e14619
Frank <frank.reimann@jambit.de>
2026-07-22 22:49:32 +0200

    remove dead code

--- assets/ajax/setTimezone.php
+++
@@ -1,7 +0,0 @@
-<?php
-	ob_start();
-	header( "Cache-Control: no-cache, must-revalidate" );
-	header( "Pragma: no-cache" );
-	header( "content-type: application/x-javascript; charset=UTF-8" );
-	setcookie("clientTimeZone", $_GET['timezone'], time() + (86400 * 30), "/");
-?>
